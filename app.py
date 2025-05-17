189e110d24d36c7adfb07418f8a2079fc0fc3f90
MossaJehad <Moosa.jehad65@gmail.com>
2025-05-17 16:21:03 +0300

    first commit: initialized project

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/ask')
+def student():
+    return 'ask endpoint and frontend interface'
+
+@app.route('/login')
+def greet():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=False, port=5000)
