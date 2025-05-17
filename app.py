0930cfbeb89979dd8b5ada4ddd9da206d9faf2f0
MossaJehad <Moosa.jehad65@gmail.com>
2025-05-17 16:37:29 +0300

    Adding 404 page

--- app.py
+++ app.py
@@ -13,5 +13,9 @@ def student():
 def greet():
     return render_template('login.html')
 
+@app.errorhandler(404)
+def not_found(e):
+	return render_template("404.html")
+
 if __name__ == '__main__':
     app.run(debug=False, port=5000)
