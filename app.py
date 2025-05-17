d8ec6681657f052da44a54063f772a83d5834fec
MossaJehad <Moosa.jehad65@gmail.com>
2025-05-17 19:29:49 +0300

    Added some better UI and fixes a lot of the python

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from transformers import pipeline
+
 app = Flask(__name__)
+#document-question-answering
+generator = pipeline("question-answering", model="distilgpt2")
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def	home():
+	return render_template('index.html')
 
-@app.route('/ask')
-def student():
-    return 'ask endpoint and frontend interface'
+@app.route('/ask', methods=['POST'])
+def	ask():
+	user_input = request.json.get('prompt', '')
+	if not user_input:
+		return jsonify({'error': 'No prompt provided'}), 400
+	res = generator(user_input, max_length=420, num_return_sequences=1)
+	return jsonify(res[0])
 
 @app.route('/login')
-def greet():
-    return render_template('login.html')
+def	login():
+	return render_template('login.html')
 
 @app.errorhandler(404)
 def not_found(e):
 	return render_template("404.html")
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000)
+	app.run(debug=True, port=5000)
